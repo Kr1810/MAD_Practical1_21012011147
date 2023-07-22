fd7bc41f27c5a47fdc574b2decb29a6927620145
Krina Suthar <98055905+Kr1810@users.noreply.github.com>
2023-07-22 21:05:46 +0530

    Practical1.4 complated

---
+++ src/main/kotlin/Practical1.3.kt
@@ -0,0 +1,30 @@
+fun main(){
+
+    println("Student Enrollment No.:")
+    var eno = readLine()!!
+    println("Student Name:")
+    var name= readLine()!!
+    println("Student Branch:")
+    var branch= readLine()!!
+    println("Student Class:")
+    var cls= readLine()!!
+    println("Student Batch:")
+    var batch= readLine()!!
+    println("Student Collage Name:")
+    var clgname= readLine()!!
+    println("Student University Name:")
+    var uni= readLine()!!
+    println("Student Age:")
+    var age= readLine()!!
+
+    println("****************")
+    println("Student's Data:" )
+    println("$eno")
+    println("$name")
+    println("$branch")
+    println("$cls")
+    println("$batch")
+    println("$clgname")
+    println("$uni")
+    println("$age")
+}
---
+++ src/main/kotlin/Practical1.4.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val a = 17
+    println("Enter the number : ${a}")
+    println("$a is ${if (a % 2 == 0) "even" else "odd"}.")
+}
