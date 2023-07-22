bdf4245ea4dbd57ce5e9e029b37bcd9696cf367c
Krina Suthar <98055905+Kr1810@users.noreply.github.com>
2023-07-22 20:59:51 +0530

    Practical1.1 complated

---
+++ src/main/kotlin/Practical1.1.kt
@@ -0,0 +1,20 @@
+fun main(){
+    var a:Int=22
+    println("integer value:$a")
+    val b:Float= 2.2F
+    println("Float value:$b")
+    val c: Char ='T'
+    println("Character value:$c")
+    var d :String="krina"
+    println("String value:$d")
+    val e:Boolean= false
+    println("Boolean value:$e")
+    val f:Double= 96.36
+    println("Double value:$f")
+    val g:Long=76578
+    println("Long value:$g")
+    val h:Short= -2
+    println("Short value:$h")
+    val i:Byte= 127
+    println("Byte value:$i")
+}
