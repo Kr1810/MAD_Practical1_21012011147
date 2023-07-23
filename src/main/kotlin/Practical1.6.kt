4171d1dd418c12ca53b805ddcbef6e2bca27de69
Krina Suthar <98055905+Kr1810@users.noreply.github.com>
2023-07-23 11:35:13 +0530

    Practical1.6 complated

---
+++ src/main/kotlin/Practical1.6.kt
@@ -0,0 +1,22 @@
+/*fun main(args: Array<String>)
+{
+    printHello()
+}
+fun printHello()
+{
+    println("Hello, World!")
+}*/
+
+fun main(){
+    var a = 111
+    var b = 2222
+    var c = -222
+    printAddition(a,b,c)
+}
+fun printAddition(a:Int , b:Int , c:Int){
+    println("Addition of 111,2222,-222 is ${a+b+c}")
+    println("Subtraction of 111,2222,-222 is ${a-b-c}")
+    println("Multiplication of 111,2222,-222 is ${a*b*c}")
+    println("Division of 111,2222,-222 is ${a/b/c}")
+}
+
