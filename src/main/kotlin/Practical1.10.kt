c899e843c0bb8edd248892be6e994fa9b8a9e72b
Krina Suthar <98055905+Kr1810@users.noreply.github.com>
2023-07-31 01:57:49 +0530

    Practical1.10 complated

--- src/main/kotlin/Practical1.10.kt
+++ src/main/kotlin/Practical1.10.kt
@@ -1,4 +1,5 @@
-class Car (info : String ,
+class
+Car (info : String ,
            Owner : String ,
            miles : Int ,
            OGPrice : Double ,
@@ -28,19 +29,19 @@ class Car (info : String ,
         println("Current Car Price:${getCurrentCarPrice()}")
         println("----------\n")
     }
-fun main() {
-    println("Creating Car Class Object car1 in next line")
-    val car1 = Car("Datsun, 2014", "ridham", 205, 20000.0, 98950.0)
-    car1.displayCarInfo()
+    fun main() {
+        println("Creating Car Class Object car1 in next line")
+        val car1 = Car("Datsun, 2014", "ridham", 205, 20000.0, 98950.0)
+        car1.displayCarInfo()
 
-    println("Creating Car Class Object car2 in next line")
-    val car2 = Car("Innova, 2019", "jigar", 20, 600000.0, 399800.0)
-    car2.displayCarInfo()
+        println("Creating Car Class Object car2 in next line")
+        val car2 = Car("Innova, 2019", "jigar", 20, 600000.0, 399800.0)
+        car2.displayCarInfo()
 
-    println("\n********** ArrayList of Car **********")
-    val carlist = ArrayList<Car>()
+        println("\n********** ArrayList of Car **********")
+        val carlist = ArrayList<Car>()
 
-    carlist.add(Car("Mahindra,2019", "meera", 150, 4900000.0, 1079000.0))
-    carlist.add(Car("Maruti,2018", "fiza", 390, 6600000.0, 4008000.0))
-    carlist[0].displayCarInfo()
-    carlist[1].displayCarInfo() }}
+        carlist.add(Car("Mahindra,2019", "meera", 150, 4900000.0, 1079000.0))
+        carlist.add(Car("Maruti,2018", "fiza", 390, 6600000.0, 4008000.0))
+        carlist[0].displayCarInfo()
+        carlist[1].displayCarInfo() }}
